87dcb7fa3abcb73302c956ae6b93a157d666a6d8
Elijah <in3rd@yahoo.com>
2017-11-23 10:53:50 -0800

    Add Lambda function code, and templating

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+import java.util.Map;
+
+public class HelloWorld {
+
+    private static final String TABLE_NAME = "Music";
+
+    public String handleRequest(Map<String, Object> input, Context context) {
+
+        return input.toString();
+    }
+}
